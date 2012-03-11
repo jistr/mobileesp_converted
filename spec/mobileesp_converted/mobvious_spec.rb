8d94391bb0e20d2890ffd981b5e8cc238cc5eb9c
Jiří Stránský <jistr@jistr.com>
2012-03-11 19:56:02 +0100

    spec stub

---
+++ spec/mobileesp_converted/mobvious_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../spec_helper'
+
+class UAgentInfoSpec < MiniTest::Spec
+  describe MobileESPConverted::UAgentInfo do
+    it "categorizes iPhone/Safari" do
+    end
+
+    it "categorizes iPad/Safari" do
+    end
+
+    it "categorizes Android phone / Android Browser" do
+    end
+
+    it "categorizes Android tablet / Android Browser" do
+    end
+
+    it "categorizes Android / Opera Mobile" do
+    end
+
+    it "categorizes Linux/Chrome" do
+    end
+
+    it "categorizes Windows/IE" do
+    end
+
+    it "categorizes Mac/Safari" do
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,4 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+begin; require 'turn'; rescue LoadError; end
+require 'mobileesp_converted'
